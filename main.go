b572eba395e8df7d1591ba2fb38517fb952ed490
Mathias Gumz <mathias.gumz@gmail.com>
2015-04-03 19:48:12 +0200

    separate package documentation into doc.go

--- main.go
+++ main.go
@@ -1,35 +1,6 @@
 // Copyright 2015 Mathias Gumz. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-// *knut* is a tiny webserver which serves folders (file trees) via user
-// specified urls. it grew a little bit beyond that scope and now serves
-// also single files and accepts file-uploads.
-//
-// usage:
-//
-//  knut [opts] uri:folder [uri2:file1] [@upload:upload_folder] [...]
-//
-// sample:
-//
-//   knut /:. /ding.txt:/tmp/dong.txt
-//
-// options:
-//
-//  -auth="": use 'name:password' to require
-//  -bind=":8080": address to bind to
-//  -compress=true: handle "Accept-Encoding" = "gzip,deflate"
-//  -log=true: log requests to stdout
-//  -server-id="knut/1.0": add "Server: <val-here>" to the response
-//  -tls-cert="": use given cert to start tls
-//  -tls-key="": use given key to start tls
-//  -tls-onetime=false: use a onetime-in-memory cert+key to drive tls
-//
-// the name:
-//
-// *knut* or "st.knut's day" is an annually celebrated festival in sweden /
-// finland on 13 January. it marks the end of christmas, among other
-// activities the christmas trees are disposed.
-//
 package main
 
 import (
